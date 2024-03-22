7967a384949e2d04f0f4c2dbf0255eba39596e06
M. JALAL <103830450+JALAL-00@users.noreply.github.com>
2024-03-22 23:34:12 +0600

    Add files via upload

---
+++ Home/Home.R
@@ -0,0 +1,8 @@
+#How to output some text, and how to do a simple calculation in R
+
+"Hello World"
+5+5
+
+#How you can use R to easily create a graph with numbers from 1 to 10 on both the x and y axis
+
+plot(1:10)
