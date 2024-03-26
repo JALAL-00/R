eae4f230896f72ff37c26b01a44b31bb19dbbc90
M. JALAL <103830450+JALAL-00@users.noreply.github.com>
2024-03-26 22:41:21 +0600

    Add files via upload

---
+++ Home/Syntext.R
@@ -0,0 +1,7 @@
+5+5
+
+print("Hello world")
+
+for (x in 1:10) {
+  print(x)
+}
---
+++ Home/Variable.R
@@ -0,0 +1,53 @@
+name <- "John"
+name
+age <- 40
+age
+
+
+name <- "John Doe"
+print(name)
+
+for (x in 1:10) {
+  print(x)
+}
+
+
+#Concatenate Elements
+
+text <- "awesome"
+paste("R is", text)
+
+text1 <- "R is"
+text2 <- "awesome"
+paste(text1, text2)
+
+
+num1 <- 5
+num2 <- 10
+num1 + num2
+
+
+num <- 5
+text <- "Some text"
+num + text
+
+#Multiple Variables
+
+# Assign the same value to multiple variables in one line
+var1 <- var2 <- var3 <- "Orange"
+
+# Print variable values
+var1
+var2
+var3
+
+#Variable Names (Identifiers)
+
+# Legal variable names:
+myvar <- "John"
+my_var <- "John"
+myVar <- "John"
+MYVAR <- "John"
+myvar2 <- "John"
+.myvar <- "John"
+
